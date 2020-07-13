e45e57ca9116fe5626e41d6b090bc373563fe07e
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-07-13 20:44:22 +0400

    Service Layer Implemented

---
+++ Todos/Todos.Services/TodoService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Todos.Core.Abstractions;
+using Todos.Core.Exceptions;
+using Todos.Core.Models;
+using Todos.Data;
+
+namespace Todos.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly TodosDbContext _context;
+
+        public TodoService(TodosDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Todo> AddAsync(Todo todo)
+        {
+            var result = await _context.Todos.AddAsync(todo);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public Task<Todo> EditAsync(Todo todo)
+        {
+            var result = _context.Update(todo);
+            return Task.FromResult(result.Entity);
+        }
+
+        public async Task<Todo> ChangeDueDateAsync(int todoId, DateTime dueDate)
+        {
+            var todo = _context.Todos.Find(todoId);
+            if (todo is null)
+                throw new TodoNotFoundException(todoId);
+
+            todo.DueDate = dueDate;
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+
+        public async Task<Todo> ChangeStartDateAsync(int todoId, DateTime startDate)
+        {
+            var todo = _context.Todos.Find(todoId);
+            if (todo is null)
+                throw new TodoNotFoundException(todoId);
+
+            todo.StartDate = startDate;
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+
+        public async Task<Todo> AssignToAsync(int todoId, int userId)
+        {
+            var todo = _context.Todos.Find(todoId);
+            if (todo is null)
+                throw new TodoNotFoundException(todoId);
+
+            todo.UserId = userId;
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+
+        public async Task<Todo> SetStatusAsync(int todoId, TodoStatus status)
+        {
+            var todo = _context.Todos.Find(todoId);
+            if (todo is null)
+                throw new TodoNotFoundException(todoId);
+
+            todo.Status = status;
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+    }
+}
---
+++ Todos/Todos.Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient.Server;
+using Microsoft.EntityFrameworkCore;
+using Todos.Core.Abstractions;
+using Todos.Core.Exceptions;
+using Todos.Core.Models;
+using Todos.Data;
+
+namespace Todos.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly TodosDbContext _context;
+
+        public UserService(TodosDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> RegisterAsync(User user)
+        {
+            var userIsAlreadyRegistered = await _context.Users.AnyAsync(u =>
+                    u.Email.Equals(user.Email, StringComparison.InvariantCultureIgnoreCase));
+
+            if (userIsAlreadyRegistered)
+                throw new UserAlreadyRegisteredException(user);
+
+            var result = await _context.Users.AddAsync(user);
+
+            await _context.SaveChangesAsync();
+
+            return result.Entity;
+        }
+
+        public Task<bool> LoginAsync(User user)
+        {
+            return LoginAsync(user.Email, user.Password);
+        }
+
+        public Task<bool> LoginAsync(string userName, string password) =>
+            _context.Users.AnyAsync(user =>
+                user.Email.Equals(userName, StringComparison.OrdinalIgnoreCase)
+                && user.Password.Equals(password, StringComparison.InvariantCulture));
+    }
+}
