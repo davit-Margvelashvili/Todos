aae26e44414162d9a89fe8e4c2247f2d0633b42a
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-07-13 20:43:33 +0400

    Exceptions Added

---
+++ Todos/Todos.Core/Exceptions/TodoNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todos.Core.Exceptions
+{
+    public class TodoNotFoundException : DomainException
+    {
+        public TodoNotFoundException(int todoId) : base($"Todo item with id {todoId} not found")
+        {
+        }
+    }
+}
---
+++ Todos/Todos.Core/Exceptions/UserAlreadyRegisteredException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Todos.Core.Models;
+
+namespace Todos.Core.Exceptions
+{
+    public class UserAlreadyRegisteredException : DomainException
+    {
+        public UserAlreadyRegisteredException(User user)
+            : base($"User '{user.Email}' has already been registered")
+        {
+        }
+    }
+}
