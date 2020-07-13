d02c26e79fc27c9c66c6d0861987708e06486af9
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-07-13 20:43:49 +0400

    Abstractions Added

---
+++ Todos/Todos.Core/Abstractions/ITodoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todos.Core.Models;
+
+namespace Todos.Core.Abstractions
+{
+    public interface ITodoService
+    {
+        Task<Todo> AddAsync(Todo todo);
+
+        Task<Todo> EditAsync(Todo todo);
+
+        Task<Todo> ChangeDueDateAsync(int todoId, DateTime dueDate);
+
+        Task<Todo> ChangeStartDateAsync(int todoId, DateTime startDate);
+
+        Task<Todo> AssignToAsync(int todoId, int userId);
+
+        Task<Todo> SetStatusAsync(int todoId, TodoStatus status);
+    }
+}
---
+++ Todos/Todos.Core/Abstractions/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Todos.Core.Models;
+
+namespace Todos.Core.Abstractions
+{
+    public interface IUserService
+    {
+        Task<User> RegisterAsync(User user);
+
+        Task<bool> LoginAsync(User user);
+
+        Task<bool> LoginAsync(string userName, string password);
+    }
+}
